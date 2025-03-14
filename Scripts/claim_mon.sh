5361264bdb5631dd7d4dff675ec6ecd9a1af8c66
Richard Szilagyi <szprichard@proton.me>
2025-03-14 13:04:14 +0000

    refactor: Replace multiple config files with .env file

--- Scripts/claim_mon.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source ./.venv/Scripts/activate
-
-export ERC20_ADDRESSES=$(cat addresses.txt)
-export DISCORD_WEBHOOK_URL=$(cat discord_webhook.txt)
-export MONAD_FAUCET_URL=$(cat monad.txt)
-
-faucet-extractor -f mon
-deactivate
